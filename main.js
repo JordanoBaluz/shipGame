e26fd3daaccdeff4fc738e1433ffd6ec68ff39da
JordanoBaluz <jordanolb95@hotmail.com>
2022-02-10 21:58:15 -0300

    implementation friend and enemys collision

--- main.js
+++ main.js
@@ -217,6 +217,16 @@ function colisao() {
         reposicionaAmigo();
         $("#amigo").remove();
     }
+    //colisão entre amigo e caminhão
+    if (colisao6.length > 0) {
+
+        amigoX = parseInt($("#amigo").css("left"));
+        amigoY = parseInt($("#amigo").css("top"));
+        explosao3(amigoX, amigoY);
+        $("#amigo").remove();
+
+        reposicionaAmigo();
+    }
 
 }
 
@@ -274,10 +284,7 @@ function explosao2(inimigo2X, inimigo2Y) {
         div2.remove();
         window.clearInterval(tempoExplosao2);
         tempoExplosao2 = null;
-
     }
-
-
 }
 
 function reposicionaAmigo() {
@@ -293,4 +300,20 @@ function reposicionaAmigo() {
             $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
         }
     }
+}
+
+function explosao3(amigoX, amigoY) {
+    $("#fundoGame").append("<div id='explosao3' class='anima4'></div");
+    $("#explosao3").css("top", amigoY);
+    $("#explosao3").css("left", amigoX);
+
+    var tempoExplosao3 = window.setInterval(resetaExplosao3, 1000);
+
+    function resetaExplosao3() {
+        $("#explosao3").remove();
+        window.clearInterval(tempoExplosao3);
+        tempoExplosao3 = null;
+
+    }
+
 }
